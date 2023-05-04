9a245babd85d03fbb3255ba352991e1123b5ae7c
cj4wilso <cj3wilso@gmail.com>
2023-05-04 17:08:13 +0100

    testing layout on mobile

--- components/layout/page/page.styles.jsx
+++ components/layout/page/page.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   align-items: center;
   padding: ${(props) => (props.type === "marketing" ? "6rem" : "1rem")};
   min-height: 100vh;
